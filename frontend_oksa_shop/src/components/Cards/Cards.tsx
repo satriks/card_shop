1f1519497bb69563df0894af19e8876c14c2b92a
Yury Vereskun <satriks@mail.ru>
2025-02-07 03:38:29 +0300

    add card, add cssVariables

--- frontend_oksa_shop/src/components/Cards/Cards.tsx
+++ frontend_oksa_shop/src/components/Cards/Cards.tsx
@@ -1,22 +1,30 @@
-import React from "react";
 import { useGetCardQuery } from "../../redux/cardAPI";
-
+import cardImg from "../../assets/testCard/card.png";
+import "./Cards.scss";
 type Props = {};
 
 export default function Cards({}: Props) {
-  const { data = [], isLoading, isError } = useGetCardQuery();
+  // const data = useGetCardQuery("");
+  const { data = [], isLoading, isError } = useGetCardQuery("");
 
   if (isLoading) return <p>Loading...</p>;
+  console.log(data, "ins data");
 
   return (
-    <div className="cards">
-      {data.map((card) => (
-        <div key={card.id} className="cardItem">
-          <h2>{card.title}</h2>
-          <img src={card.imageUrl} alt={card.title} />
-          <p>{card.content}</p>
-        </div>
-      ))}
+    <div className="cards_wrapper">
+      <h2>Открытки темы Title :</h2>
+      <div className="cards">
+        {data.map((card) => (
+          <div key={card.id} className="cardItem">
+            <img src={cardImg} alt={card.title} />
+            <h2>{card.title}</h2>
+            <div className="cardItem_price">
+              <button>В корзину</button>
+              <p>2000 Р</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
