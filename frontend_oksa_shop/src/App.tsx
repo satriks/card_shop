615b81193a5da2397d970ed44b31255794dee603
Yury Vereskun <satriks@mail.ru>
2025-02-13 00:47:44 +0300

    add cart html, css

--- frontend_oksa_shop/src/App.tsx
+++ frontend_oksa_shop/src/App.tsx
@@ -14,6 +14,7 @@ import { useEffect } from "react";
 import Spinner from "./components/Common/Spiner/Spinner";
 import { setCards, setCategory } from "./redux/MainSlice";
 import { CardDataDto } from "./models/models";
+import Cart from "./components/Cart/Cart";
 
 function App() {
   const { data = [], isLoading, isError } = useGetCardQuery("");
@@ -35,6 +36,7 @@ function App() {
     <div className="wrapper">
       <Header />
       <Title />
+      <Cart />
       <Category />
       {isLoading ? <Spinner /> : <Cards />}
       <AboutMe />
